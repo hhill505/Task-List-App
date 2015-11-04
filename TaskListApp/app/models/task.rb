5943eeafc19bc89836271d4ffaec343e97b3c849
Henry Hill <hhill1@nd.edu>
2015-11-03 22:21:15 -0500

    Fails to add associated classes to show and index pages

--- TaskListApp/app/models/task.rb
+++ TaskListApp/app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
 	belongs_to :task_list;
+
+	
 end
--- TaskListApp/app/models/task_list.rb
+++ TaskListApp/app/models/task_list.rb
@@ -1,7 +1,4 @@
 class TaskList < ActiveRecord::Base
 	has_many :tasks
 
-	def name
-		"#{Name.capitalize}"
-	end
 end
