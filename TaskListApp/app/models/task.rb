7c05e82dc4d1bd3101c344273ddd703fd2a0f314
Henry Hill <hhill1@nd.edu>
2015-11-09 19:50:52 -0500

    validations

--- TaskListApp/app/models/task.rb
+++ TaskListApp/app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :tasklist
+  validates :title, :priorit, :duedate, :tasklist, presence: true
+  validates :priorit, numericality: {greater_than_or_equal_to: 1}
+  validates :priorit, numericality: {less_than_or_equal_to: 10}
 end
